74350adf2db78524519e6463ab3c1e1e9174a6ae
jonas <git@jonaz.dev>
2020-11-16 18:55:24 +0100

    Rename PlayerListener

--- src/main/kotlin/com/vacegaming/james/musicbot/listener/GuildMessageReceivedLister.kt
+++ src/main/kotlin/com/vacegaming/james/musicbot/listener/GuildMessageReceivedLister.kt
@@ -1,10 +1,12 @@
 package com.vacegaming.james.musicbot.listener
 
+import com.vacegaming.james.musicbot.util.config.ConfigManager
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 
-class PlayListener : ListenerAdapter() {
+object GuildMessageReceivedLister : ListenerAdapter() {
+
     override fun onGuildMessageReceived(event: GuildMessageReceivedEvent) {
-        // TODO: not yet implemented
+        if (event.message.channel.idLong != ConfigManager.data.musicBotChannelID) return
     }
 }
