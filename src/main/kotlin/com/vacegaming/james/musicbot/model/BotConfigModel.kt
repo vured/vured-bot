6ed5b2e61388fb801dd4a4dcde7c3dc095b56a08
jokuni <2757922-jokuni@users.noreply.gitlab.com>
2020-11-25 19:22:28 +0100

    Fix Music Bot environment

--- src/main/kotlin/com/vacegaming/james/musicbot/model/BotConfigModel.kt
+++ src/main/kotlin/com/vacegaming/james/musicbot/model/BotConfigModel.kt
@@ -9,5 +9,5 @@ data class BotConfigModel(
     val vaceGamingGuildId: Long,
     val maxPlaylistTracks: Int,
     val defaultMessage: String,
-    var botToken: String,
+    var botToken: String?,
 )
