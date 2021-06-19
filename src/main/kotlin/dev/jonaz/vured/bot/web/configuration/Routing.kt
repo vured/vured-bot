54dce8236b86c232d82d26caa2db274084622f07
jonaznas <git@jonaz.dev>
2021-06-19 15:10:18 +0200

    feat(web): user endpoint

--- src/main/kotlin/dev/jonaz/vured/bot/web/configuration/Routing.kt
+++ src/main/kotlin/dev/jonaz/vured/bot/web/configuration/Routing.kt
@@ -2,6 +2,7 @@ package dev.jonaz.vured.bot.web.configuration
 
 import dev.jonaz.vured.bot.persistence.web.UserPrincipal
 import dev.jonaz.vured.bot.web.route.index
+import dev.jonaz.vured.bot.web.route.user
 import dev.jonaz.vured.bot.web.route.verification
 import io.ktor.application.*
 import io.ktor.auth.*
@@ -13,10 +14,6 @@ fun Application.configureRouting() = routing {
     verification()
 
     authenticate {
-        get("/welcome") {
-            val principal = call.authentication.principal<UserPrincipal>()
-            val name = principal?.name
-            call.respondText("Hello, $name!")
-        }
+        user()
     }
 }
---
+++ src/main/kotlin/dev/jonaz/vured/bot/web/route/User.kt
@@ -0,0 +1,21 @@
+package dev.jonaz.vured.bot.web.route
+
+import dev.jonaz.vured.bot.persistence.web.UserPrincipal
+import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.http.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import kotlinx.serialization.Serializable
+
+fun Route.user() {
+    get("/user") {
+        val principal = call.authentication.principal<UserPrincipal>()
+
+        if(principal == null) {
+            call.respond(HttpStatusCode.NotFound)
+        } else {
+            call.respond(principal)
+        }
+    }
+}
