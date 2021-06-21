0571de7c912ef15e590992efe67ad99aea23dc40
jonaznas <git@jonaz.dev>
2021-06-21 04:53:48 +0200

    feat(web): add player websocket endpoint

--- src/main/kotlin/dev/jonaz/vured/bot/web/configuration/Routing.kt
+++ src/main/kotlin/dev/jonaz/vured/bot/web/configuration/Routing.kt
@@ -1,17 +1,18 @@
 package dev.jonaz.vured.bot.web.configuration
 
-import dev.jonaz.vured.bot.persistence.web.UserPrincipal
 import dev.jonaz.vured.bot.web.route.index
+import dev.jonaz.vured.bot.web.route.player
 import dev.jonaz.vured.bot.web.route.user
 import dev.jonaz.vured.bot.web.route.verification
 import io.ktor.application.*
 import io.ktor.auth.*
-import io.ktor.response.*
 import io.ktor.routing.*
 
 fun Application.configureRouting() = routing {
     index()
     verification()
+
+    player()
 
     authenticate {
         user()
